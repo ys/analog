36769637f75f83fd9d1471b5bc5dadc4ce15f581
Yannick Schutz <hello@yannickschutz.com>
2021-03-25 22:56:52 +0100

    Spec the contact sheet

--- spec/features/roll_spec.rb
+++ spec/features/roll_spec.rb
@@ -8,10 +8,26 @@ RSpec.describe "Roll" do
     Analog::Config.path = @dir
   end
 
-  it "works" do
+  let(:roll) { Analog::Roll.first }
+
+  it "reads the roll.md" do
     expect(Analog::Roll.all.size).to eql(1)
-    expect(Analog::Film.all.size).to eql(1)
-    expect(Analog::Camera.all.size).to eql(1)
+  end
+
+  it "converts to html" do
+    sheet = roll.to_html
+    expect(sheet).to include(roll.id)
+    expect(sheet).to include(roll.camera.to_s)
+    expect(sheet).to include(roll.film.to_s)
+  end
+
+  it "creates a contact sheet" do
+    sheet_path = File.join(@dir, "roll-1", "roll.html")
+    expect do
+      roll.add_contact_sheet
+    end.to change { File.exist? sheet_path }.to(true)
+    sheet = File.read(sheet_path)
+    expect(sheet).to eql(roll.to_html)
   end
 
   after do
